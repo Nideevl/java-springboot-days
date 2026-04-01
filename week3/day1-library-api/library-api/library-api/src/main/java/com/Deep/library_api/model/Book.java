99f271c78134fac56cf0cfa84a42b4b830269402
Deepak Singh Bungla <nideevl4574@gmail.com>
2026-04-01 19:17:55 +0530

    Week 3 Day 3 - Spring Data JPA, PostgreSQL integration

--- week3/day1-library-api/library-api/library-api/src/main/java/com/Deep/library_api/model/Book.java
+++ week3/day1-library-api/library-api/library-api/src/main/java/com/Deep/library_api/model/Book.java
@@ -1,3 +1,39 @@
 package com.Deep.library_api.model;
 
-public record Book(long id, String title, String author) {}
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private String genre;
+    private boolean available;
+
+    public Book() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+}
--- week3/day1-library-api/library-api/library-api/src/main/java/com/Deep/library_api/repository/BookRepository.java
+++ week3/day1-library-api/library-api/library-api/src/main/java/com/Deep/library_api/repository/BookRepository.java
@@ -1,18 +1,7 @@
 package com.Deep.library_api.repository;
 
 import com.Deep.library_api.model.Book;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.sql.Connection;
-import java.util.List;
-
-@Repository
-public class BookRepository {
-
-    public List<Book> findAll() {
-        return List.of(
-                new Book(1L, "Clean Code", "Robert Martin"),
-                new Book(2L, "Effective Java", "Joshua Bloch")
-        );
-    }
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
